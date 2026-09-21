bc3130954f576acbd0e5b701686848489081d584
Kulusienko <brzydkikartofel3@gmail.com>
2026-09-21 20:11:56 +0200

    02_mobilne – projekt C# .NET MAUI NotatkiMobilne

---
+++ 02_mobilne/NotatkiMobilne/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+
+namespace NotatkiMobilne;
+
+public partial class MainPage : ContentPage
+{
+    public ObservableCollection<string> Notatki { get; set; }
+
+    public MainPage()
+    {
+        InitializeComponent();
+
+        Notatki = new ObservableCollection<string>
+        {
+            "Notatka pierwsza",
+            "Notatka 2",
+            "Trzecia notatka"
+        };
+
+        BindingContext = this;
+    }
+
+    private void DodajNotatke_Clicked(object sender, EventArgs e)
+    {
+        string tekst = NotatkaEntry.Text?.Trim();
+
+        if (!string.IsNullOrWhiteSpace(tekst))
+        {
+            Notatki.Add(tekst);
+            NotatkaEntry.Text = string.Empty;
+        }
+    }
+}
